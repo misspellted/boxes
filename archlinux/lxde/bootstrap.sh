4c554583c309f166af9615a879de8680af316a63
James Goodrich <16412448+misspellted@users.noreply.github.com>
2020-08-09 15:19:50 -0500

    Add basic Arch Linux + LXDE environment (#1)
    * Adds ticketted-dev environment [WIP]
    
    Switched to Arch Linux from CentOS/7. Seems to work a little better? Still not
    quite happy with the order of operations for VirtualBox Guest Additions, but I
    will take what I can get at the moment. Boots to LXDE desktop with the vagrant
    user. Would like to change in the near future, but that may involve referencing
    a configuration script that is excluded from the repository (secrets and all).
    
    * Disassociate from ticketted project
    
    The repository's focus has shifted from specific environments to collecting the
    'base' environments - those environments which are then additionally customized
    for a particular project.
    
    I feel a Packer pipeline is in the future. How near? Only the future knows.
    
    * Granulates the ArchLinux envs to include DE
    
    In this particular case, I've been testing/looking to use LXDE, so include that
    kind of information in the path. If, in the future, a GNOME-based ArchLinux env
    is wanted, I would expect a similar pair of files, under an archlinux/gnome
    path.

---
+++ archlinux/lxde/bootstrap.sh
@@ -0,0 +1,27 @@
+# Update installed packages
+yes | pacman -Syu
+
+# Install the Linux headers for VirtualBox
+yes | pacman -S linux-headers
+
+# Assist in locating additional packages
+yes | pacman -S pacman-contrib
+
+# Use a simple desktop environment
+# Since pacman allows for custom installation of a group,
+#   the default is what we want (for now), and the input
+#   is not a simple 'y', we're using the --noconfirm flag.
+pacman -S --noconfirm xorg xorg-server xorg-xinit lxde
+
+# Configure the desktop environment to run
+sudo systemctl start lxdm.service
+sudo systemctl enable lxdm.service
+
+# Replace virtualbox-guest-utils-nox
+yes | pacman -R virtualbox-guest-utils-nox
+yes | pacman -S virtualbox-guest-utils --overwrite "*"
+
+# Refresh VirtualBox
+VBoxClient-all
+
+echo "Finished bootstrapping!"
