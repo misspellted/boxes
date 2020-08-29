8e5bc75a343b59d9c2dcea7d675507c1c136333d
James Goodrich <jdgoodrichjr@gmail.com>
2020-08-29 04:04:44 -0500

    Provides additional script for ... additional actions

---
+++ archlinux/lxde/additional.sh
@@ -0,0 +1,4 @@
+# Provides a script file for additional provisioning actions by a user of the box.
+# For example, adding additional packages useful in a development environment.
+# Of course, the commands included by those processes are not limited. Be careful.
+echo "Provisioning additional actions..."
